4a0cd8a21580c94099d599a3df34e7e427f78629
Jackson Henry <jackson.henry13@yahoo.com>
2019-06-04 19:48:57 -0500

    Added the first potential friend in the friends.js file. This friend is Dell CEO Michael Dell. I am inputting random values for the agree/disagree questions.

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+//This file is where we will store the array of all of our friends
+var techFriends = [
+    {
+        name: "Michael Dell",
+        photo: "https://i.dell.com/sites/content/corporate/bios/en/PublishingImages/Dell%20New%201.jpg",
+        scores: ['3', '5', '2', '4', '3', '1', '2', '3', '5', '4']
+    }
+]
