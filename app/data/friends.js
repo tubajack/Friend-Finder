913d1dd00c7ccf9af714f55f10e5081fe590b57c
Jackson Henry <jackson.henry13@yahoo.com>
2019-06-04 20:06:24 -0500

    Added my fifth and final tech CEO. Amazon is definitely a company I would mention when I discuss large tech companies. It's CEO Jeff Bezos is the world's richest man.

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,21 +3,26 @@ var techFriends = [
     {
         name: "Michael Dell",
         photo: "https://i.dell.com/sites/content/corporate/bios/en/PublishingImages/Dell%20New%201.jpg",
-        scores: ['3', '5', '2', '4', '3', '1', '2', '3', '5', '4']
+        scores: ['3','5','2','4','3','1','2','3','5','4']
     },
     {
         name: "Elon Musk", 
         photo: "https://si.wsj.net/public/resources/images/BN-SS512_2YjoP_OR_20170330060433.jpg?width=620&height=415",
-        scores: ['5', '1', '2', '1', '1', '5', '5', '2', '1', '3']
+        scores: ['5','1','2','1','1','5','5','2','1','3']
     },
     {
         name: "Ginni Rometty",
         photo: "https://ei.marketwatch.com/Multimedia/2017/02/13/Photos/ZH/MW-FF802_ibm021_20170213200120_ZH.jpg?uuid=18ecf31c-f251-11e6-a740-001cc448aede",
-        scores: ['3', '4', '2', '5', '4', '5', '3', '1', '2', '4']
+        scores: ['3','4','2','5','4','5','3','1','2','4']
     },
     {
         name: "Satya Nadella", 
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5NqNXDrcxxPyIUEB7513e344_t7d_WLY3jT9QOuu0KCpfdQcD", 
         scores: ['2','3','5','5','4','3','4','5','5','4']
+    },
+    {
+        name: "Jeff Bezos",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2sjmy9cph4ZbQIv2eMsn-3Yla-pIBrFTQ86Xot3kTDHoHXUL8Rg",
+        scores: ['1','3','4','3','2','5','5','4','2','3']
     }
 ];
