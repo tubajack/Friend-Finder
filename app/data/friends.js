39403243c11ffc7a97c20a1b3f904e2d1f623559
Jackson Henry <jackson.henry13@yahoo.com>
2019-06-04 19:53:31 -0500

    Added the second tech CEO. This CEO is Elon Musk from Tesla.

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,5 +4,10 @@ var techFriends = [
         name: "Michael Dell",
         photo: "https://i.dell.com/sites/content/corporate/bios/en/PublishingImages/Dell%20New%201.jpg",
         scores: ['3', '5', '2', '4', '3', '1', '2', '3', '5', '4']
+    },
+    {
+        name: "Elon Musk", 
+        photo: "https://si.wsj.net/public/resources/images/BN-SS512_2YjoP_OR_20170330060433.jpg?width=620&height=415",
+        scores: ['5', '1', '2', '1', '1', '5', '5', '2', '1', '3']
     }
 ]
