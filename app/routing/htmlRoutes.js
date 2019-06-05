66de848516255f9f0747ff1f7da74037ba1b00e5
Jackson Henry <jackson.henry13@yahoo.com>
2019-06-04 22:20:24 -0500

    Set up the get request for when the user visits the survey.html page.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,12 @@
 //Include the path package to get the correct file path for the html
 var path = require("path");
+
+//Routing time
+module.exports = function(apply){
+    //In this function, we will have HTML get requests
+    //When the user visits a webpage, this below code handles what will happen. 
+
+    apply.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+}
